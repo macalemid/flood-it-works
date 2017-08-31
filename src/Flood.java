ab2e39728d605411cfd1c5e68e8d77475baed733
Ahmed <mikahmed@ads.iu.edu>
2017-08-31 11:56:53 -0400

    IT WORKS ON ECLIPSEgit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .

---
+++ src/Flood.java
@@ -0,0 +1,16 @@
+import java.util.List;
+import java.util.Map;
+
+public class Flood {
+
+    // Students implement this flood function.
+
+    public static void flood(WaterColor color,
+			     List<Coord> flooded_list,
+			     Map<Coord,Tile> tiles,
+			     Integer board_size)
+    {
+	
+    }
+
+}
