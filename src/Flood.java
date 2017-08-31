78f9376ff6333348130f725ea18fd0a19a1a0e65
Ahmed <mikahmed@ads.iu.edu>
2017-08-31 12:34:03 -0400

    broken code

--- src/Flood.java
+++ src/Flood.java
@@ -8,9 +8,34 @@ public class Flood {
     public static void flood(WaterColor color,
 			     List<Coord> flooded_list,
 			     Map<Coord,Tile> tiles,
-			     Integer board_size)
-    {
-	
+			     Integer board_size) {
+    	
+    	System.out.println(flooded_list.size());
+    
+    for(int i = 0; i < flooded_list.size(); i++){
+    	
+    	tiles.get(flooded_list.get(i)).setColor(color);
+    	
+    	
+    }
+    
+    	for(int i = 0; i < flooded_list.size(); i++){
+    		
+    List<Coord> friends = flooded_list.get(i).neighbors(board_size);
+    
+    for(int j = 0; j <friends.size(); j++) {
+    	
+    	Tile current = tiles.get(friends.get(j));
+    	
+    	if( current.getColor().equals(color)){
+    		
+    		current.setColor(color);
+    		
+    		flooded_list.add(friends.get(j));
+    	}
+    }
+    	}
+    	
     }
 
 }
